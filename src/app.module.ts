70fe8e8eca0151026d60ef53416d441437b58ac8
Andrei <mojitoboss@gmail.com>
2023-03-05 19:40:31 +0300

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { UserModule } from './modules/user/user.module';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb://root:czP08OfyCIT1kqYwhjMqQMv1@sulikoks.ru:30101/vdict')
+  ],
   controllers: [AppController],
   providers: [],
 })
